0cb28e236bbb748fc44ccf6cec623a2cc03e64fa
Chris Chao <chris_chao@trend.com.tw>
2016-02-15 21:40:54 +0800

    Add resume contents in JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,77 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var work = {
+  "jobs": [
+    {
+      "title": "Senior Engineer",
+      "employer": "Trend Micro Inc.",
+      "dates": "2015 - 2016",
+      "location": "Taipei",
+      "description": "Web Developer"
+    },
+    {
+      "title": "Engineer",
+      "employer": "Trend Micro Inc.",
+      "dates": "2013 - 2015",
+      "location": "Taipei",
+      "description": "Windows/Mac Developer"
+    }
+  ]
+};
+
+var projects = {
+  "projects": [
+    {
+      "title": "SafeSync For Enterprise",
+      "dates": "2013 - 2016",
+      "description": "An on-premise file syncing and sharing solution for enterprises.",
+      "images": [
+        "http://placehold.it/200x200",
+        "http://placehold.it/200x200"
+      ]
+    }
+  ]
+};
+
+var bio = {
+  "name": "Shu-Bo Chao",
+  "role": "Front-End Ninja",
+  "welcomeMessage": "Hello! I'm a developer who likes to create great websites!",
+  "contacts": {
+    "mobile": "",
+    "email": "apiggy0204@gmail.com",
+    "github": "apiggy0204",
+    "twitter": "apiggy0204",
+    "location": "Taipei"
+  },
+  "skills": [
+    "Node.js (Express)",
+    "Responsive Web Design (Bootstrap)",
+    "JS Framework (Angular, React)",
+    "Windows/Mac development"
+  ]
+};
+
+var education = {
+  "schools": [
+    {
+      "name": "National Taiwan University",
+      "location": "Taipei",
+      "degree": "MS",
+      "majors": [
+        "CS"
+      ],
+      "dates": "2011 - 2013",
+      "url": "http://www.ntu.edu.tw"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Machine Learning - Stanford University",
+      "school": "Coursera",
+      "dates": "2016",
+      "url": "https://www.coursera.org/learn/machine-learning"
+    }
+  ]
+};
